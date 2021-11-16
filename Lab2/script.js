c3689388d68804b42826ad2fb8ea6153b51b8956
Marcin Klisz <marcin.klisz@icloud.com>
2021-11-17 00:36:12 +0100

    add input check; add feature 11-14

--- Lab2/script.js
+++ Lab2/script.js
@@ -43,6 +43,9 @@ function add_product_to_list() {
         shopping_list = document.createElement('ul');
     }
     li = document.createElement('li');
+    if(document.getElementById('product_name').value == ''){
+        return;
+    }
     li.appendChild(document.createTextNode(document.getElementById('product_name').value));
     shopping_list.appendChild(li);
     element_after = document.getElementsByTagName('p')[2];
@@ -51,4 +54,39 @@ function add_product_to_list() {
 
 function change_background_color(){
     document.body.style.backgroundColor = '#'+(0x1000000+Math.random()*0xffffff).toString(16).substr(1,6);
+}
+
+function change_bg_color() {
+    document.getElementById('square').style.backgroundColor = '#'+(0x1000000+Math.random()*0xffffff).toString(16).substr(1,6);
+}
+
+function change_title(){
+    title = document.getElementsByClassName("navbar-brand")[0];
+    if(document.getElementById('title_val').value == ''){
+        return;
+    }
+    title.innerHTML = document.getElementById('title_val').value;
+}
+
+function draw_figure(){
+    width = document.getElementById('width').value;
+    height = document.getElementById('height').value;
+
+    if (document.getElementById('figure') != null){
+        document.getElementsByClassName('feature')[0].removeChild(document.getElementById('figure'))
+    }
+
+    figure = document.createElement('div');
+    figure.id = 'figure';
+    figure.style.width = width.toString()+'px';
+    figure.style.height = height.toString()+'px';
+    figure.style.border = '4px solid black';
+    document.getElementsByClassName("feature")[0].appendChild(figure);
+}
+
+function figure_fill(){
+    draw_figure()
+    color = document.getElementById('color_fill').value;
+    figure = document.getElementById('figure');
+    figure.style.backgroundColor = color;
 }
